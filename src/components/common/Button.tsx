0fe3c9c13682dc20be00fc0238a1f63e3d4540e3
six-standard <dbrrl1127@gmail.com>
2024-05-19 23:20:17 +0900

    chore: 버튼 컴포넌트 옵션 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,21 +4,37 @@ interface PropType
   extends Partial<Omit<HTMLButtonElement, "children" | "onClick">> {
   children: string;
   icon: string;
+  size?: {
+    px: number;
+    py: number;
+    text: number;
+    icon: number;
+    gap: number;
+  };
   onClick?: () => void;
+  selected?: boolean;
+  disabled?: boolean;
 }
 
 export const Button = ({
   children,
   icon,
+  size = { px: 4, py: 2, text: 20, icon: 20, gap: 3 },
   onClick = () => console.log("click!"),
+  selected,
+  disabled,
 }: PropType) => {
   return (
     <button
       onClick={onClick}
-      className="flex items-center gap-3 px-4 py-2 border-[#2c2c2c] border-[3px] hover:opacity-50 transition-all duration-200 ease-in-out"
+      className={`flex items-center ${`gap-${size.gap}`} ${`px-${size.px}`} ${`py-${size.py}`} border-[#2c2c2c] border-[3px] ${
+        selected || (disabled && "opacity-50")
+      } hover:opacity-50 transition-all duration-200 ease-in-out`}
     >
-      <Icon icon={icon} width={20} color="#2c2c2c" />
-      <span className="font-['WantedSansB'] text-[20px] text-[#2c2c2c]">
+      <Icon icon={icon} width={size.icon} color="#2c2c2c" />
+      <span
+        className={`font-['WantedSansB'] ${`text-[${size.text}px]`} text-[#2c2c2c]`}
+      >
         {children}
       </span>
     </button>
