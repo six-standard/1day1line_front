322538ea47a7e81fa5233af5546be4fc720e9983
six-standard <dbrrl1127@gmail.com>
2024-06-01 23:29:13 +0900

    chore: 적당히 필요한 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/static/Header";
 import "./index.css";
 import { Home } from "./pages/Home";
+import { Onboard } from "./pages/Onboard";
 
 function App() {
   return (
     <>
+      {/* <Onboard /> */}
       <Header />
       <Home />
     </>
