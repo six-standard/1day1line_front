41a7bca845a17d1b6932d965db61bc86748950f5
six-standard <dbrrl1127@gmail.com>
2024-06-02 22:56:10 +0900

    chore: 필요없는 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Header } from "./components/static/Header";
 import "./index.css";
 import { Home } from "./pages/Home";
-import { Onboard } from "./pages/Onboard";
 
 function App() {
   return (
     <>
-      {/* <Onboard /> */}
       <Header />
       <Home />
     </>
