f0e39dc3ac9eed82a2bb8ea283c7a9454ae50884
six-standard <dbrrl1127@gmail.com>
2024-06-05 23:21:00 +0900

    refactor: 일부 파일 분리

---
+++ src/pages/Home/List/components/Item.tsx
@@ -0,0 +1,26 @@
+import { Detail } from "../../../../components/common/modal/modals/Detail";
+import { useModal } from "../../../../hooks/useModal";
+
+interface PropType {
+  date: number[];
+  day: string;
+  content: string;
+}
+
+export const Item = ({ date, day, content }: PropType) => {
+  const { setModal } = useModal();
+
+  return (
+    <div
+      className="cursor-pointer w-[70%] h-[75px] flex items-center border-[3px] border-[#2C2C2C]"
+      onClick={() => setModal(<Detail date={date} />)}
+    >
+      <div className="text-center content-center border-r-[3px] border-r-[#2C2C2C] h-full w-[100px] font-[WantedSansEB] text-[20px]">
+        {date[2] + ". " + day}
+      </div>
+      <div className="w-full px-5 font-[WantedSansM] text-[20px] text-ellipsis whitespace-nowrap overflow-hidden">
+        {content}
+      </div>
+    </div>
+  );
+};
