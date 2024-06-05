5e0383b1c2d8a9fc873f8be97c6b189b62055769
six-standard <dbrrl1127@gmail.com>
2024-06-05 23:20:13 +0900

    chore: 토큰 없으면 온보드 페이지로 리다이렉트

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,22 @@
-import { List } from "./List";
+import { useEffect } from "react";
 import { Write } from "./Write";
+import { Cookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { List } from "./List";
+
+const cookie = new Cookies();
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!cookie.get("accessToken")) {
+      navigate("/onboard");
+    }
+  }, []);
+
   return (
-    <div className="w-[100%] h-[100vh] inline-flex overflow-x-hidden">
+    <div className="w-[100%] h-[100vh] inline-flex overflow-hidden">
       <Write />
       <List />
     </div>
