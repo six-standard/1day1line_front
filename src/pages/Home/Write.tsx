5e98399beecc7ac3ffcd3846bbd09ac6c1bce635
six-standard <dbrrl1127@gmail.com>
2024-06-05 23:20:33 +0900

    chore: 작성 페이지 타임스탬프 추가

--- src/pages/Home/Write.tsx
+++ src/pages/Home/Write.tsx
@@ -1,19 +1,41 @@
+import { ChangeEvent, useState } from "react";
 import { Button } from "../../components/common";
 import { days } from "../../utils/date";
 import dayjs from "dayjs";
 const date = dayjs();
 
 export const Write = () => {
+  const [content, setContent] = useState("");
+
   return (
-    <main className="w-full h-[100vh] flex justify-center items-center relative shrink-0">
+    <main className="w-[100%] h-[100vh] flex justify-center items-center shrink-0">
       <div className="flex flex-col gap-3 w-[70%] max-w-[1020px]">
         <input
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setContent(e.target.value)
+          }
+          value={content}
           placeholder="일기를 작성해주세요..."
           className="bg-transparent outline-none border-[3px] border-[#2c2c2c] w-full p-3 px-5 text-[20px]"
         />
         <div className="flex w-full items-center justify-between">
           <div className="flex items-center gap-3 select-none">
-            <Button icon="mingcute:time-fill">타임스탬프</Button>
+            <Button
+              icon="mingcute:time-fill"
+              onClick={() =>
+                setContent(
+                  `${`${new Date()
+                    .getHours()
+                    .toString()
+                    .padStart(2, "0")}:${new Date()
+                    .getMinutes()
+                    .toString()
+                    .padStart(2, "0")} `}` + content
+                )
+              }
+            >
+              타임스탬프
+            </Button>
             <Button icon="material-symbols:check">작성 완료</Button>
           </div>
           <div className="flex items-center gap-1 border-[3px] px-2 py-1 border-[#2c2c2c]">
