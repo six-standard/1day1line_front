89e08cbc42df0b6db2335bc6a8dd8789c6463177
six-standard <dbrrl1127@gmail.com>
2024-06-11 20:22:47 +0900

    feat: 쿼리클라이언트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import { Router } from "./router";
 
+const queryclient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryclient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
